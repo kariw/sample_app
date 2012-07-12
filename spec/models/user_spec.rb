c5e8b44c0f677681d6a4af1c8352c22699c692a9
Kari Weiler <k.f.weiler@gmail.com>
2012-07-11 19:50:42 -0700

    Update email downcasing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -74,6 +74,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
