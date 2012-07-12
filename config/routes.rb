3a041792f50a72af6b07dd978aad286af457a687
Kari Weiler <k.f.weiler@gmail.com>
2012-07-11 23:58:41 -0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
-  
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
