ddb8484e9eed2ab7305f3b25d213490a2c91b4ff
Kari Weiler <k.f.weiler@gmail.com>
2012-07-17 01:02:52 -0700

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,4 +6,12 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
 
   default_scope order: 'microposts.created_at DESC'
+
+  # Returns microposts from the users being followed by the given user.
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", 
+          user_id: user.id)
+  end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -52,6 +52,31 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }          
+        end
+      end
+
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
@@ -59,6 +84,13 @@ describe "Authentication" do
           fill_in "Password", with: user.password
           click_button "Sign in"
         end
+
+        describe "after signing in" do
+
+          it "should render the desired protected page" do
+            page.should have_selector('title', text: 'Edit user')
+          end
+      end
 
       describe "in the Microposts controller" do
 
@@ -71,13 +103,6 @@ describe "Authentication" do
           before { delete micropost_path(FactoryGirl.create(:micropost)) }
           specify { response.should redirect_to(signin_path) }
         end
-      end
-
-        describe "after signing in" do
-
-          it "should render the desired protected page" do
-            page.should have_selector('title', text: 'Edit user')
-          end
 
           describe "when signing in again" do
             before do
